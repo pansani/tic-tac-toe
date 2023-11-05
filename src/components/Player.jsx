b3ddd8b168b974385b77d045da1bb9ae39b1ae7c
Joao Claudio <pansanijoaomendonca@gmail.com>
2023-11-05 16:17:57 -0300

    create GameBoard component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,23 +4,30 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setisEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
     setisEditing((editing) => !editing);
+  } //if your state depends on your previous state, pass a function to your state when updating a function
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
-  1;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
@@ -31,4 +38,5 @@ export default function Player({ name, symbol }) {
 Player.propTypes = {
   name: PropTypes.string.isRequired,
   symbol: PropTypes.string.isRequired,
+  initialName: PropTypes.string.isRequired,
 };
